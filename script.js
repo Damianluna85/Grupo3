0a77d4d44ec7923a52dba85fb512badf088c3b9d
Damianluna85 <83967956+Damianluna85@users.noreply.github.com>
2023-10-23 16:02:39 -0300

    con Js

---
+++ script.js
@@ -0,0 +1,37 @@
+let header = `
+<a  href="index.html">
+            <img src="IMGS/sunsea.png" alt="Mundo">
+            Green Solutions
+        </a>
+        <nav>
+            <a href="index.html">Inicio</a>
+            <a href="#">Soluciones</a>
+            <a href="#">Nuestros Clientes</a>
+            <a href="contacto.html" target="_blank">Contacto</a>
+        </nav>
+`
+ document.getElementById ("idheader").innerHTML = header;
+
+ let footer =`
+ 
+        <a href="index.html">
+            <img src="imgs/sunsea-wht.png" alt="Summer Exploring Logo">
+            Green Solutions
+        </a>
+
+        <div>
+
+            <a href="">
+                <img src="IMGS/instagram-w 1.png" alt="Instagram">
+            </a>
+            <a href="">
+                <img src="IMGS/facebook-w 1.png" alt="Facebook">
+            </a>
+            <a href="">
+                <img src="IMGS/twitter-w 1.png" alt="Twitter">
+            </a>
+            
+        </div>
+ `
+
+ document.getElementById("idfooter").innerHTML = footer;
